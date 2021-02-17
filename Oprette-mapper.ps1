6a78b7266828437feb5bea4145a9ad6b490f3e72
Mads80 <58600076+Mads80@users.noreply.github.com>
2021-02-17 15:06:06 +0100

    Update Oprette-mapper.ps1

--- Oprette-mapper.ps1
+++ Oprette-mapper.ps1
@@ -2,7 +2,6 @@
 new-item -type directory -path C:\Byer\Odense\Marketing -Force
 new-item -type directory -path C:\Byer\Odense\Produktion -Force
 
-
 new-item -type directory -path C:\Byer\Vejle\Salg -Force
 new-item -type directory -path C:\Byer\Vejle\Marketing -Force
 new-item -type directory -path C:\Byer\Vejle\Produktion -Force
