a8d21bebe59258f9a5f3f70804d756ad81b7d5d9
Mads80 <58600076+Mads80@users.noreply.github.com>
2021-02-18 13:00:42 +0100

    files

---
+++ Ansatte.ps1
@@ -0,0 +1,13 @@
+﻿$Users = Import-Csv -Delimiter "," -Path "C:\Scripts\Ansatte.csv"        
+foreach ($User in $Users)
+{               
+    $UserFirstname = $User.FirstName       
+    $UserLastname = $User.LastName
+    $UserMail = $user.Mail
+    $UserPassword = $User.Password
+    $UserDescription = $User.Description
+    $UserOffice = $user.Office
+    New-ADUser -Name "$UserFirstname" -Office "$UserOffice" -DisplayName "$UserFirstname" -EmailAddress "$UserMail" -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$UserDescription" -AccountPassword (ConvertTo-SecureString $UserPassword -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $true -server jmm.local
+    
+    echo ("Brugeren "+ $UserFirstname +" er blevet tilføjet til AD.")      
+}
